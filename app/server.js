f5dedce0d7fd6145dcf42d8a1d650b4be6c07950
David Herron <david@davidherron.com>
2020-03-21 15:45:20 -0700

    Initial Revision

---
+++ app/server.js
@@ -0,0 +1,25 @@
+
+const http = require('http');
+const util = require('util');
+const url  = require('url');
+const os   = require('os');
+
+const server = http.createServer();
+server.on('request', (req, res) => {
+    res.end(
+`<html><head><title>Operating System Info</title></head>
+<body><h1>Operating System Info</h1>
+<table>
+<tr><th>TMP Dir</th><td>${os.tmpdir()}</td></tr>
+<tr><th>Host Name</th><td>${os.hostname()}</td></tr>
+<tr><th>OS Type</th><td>${os.type()} ${os.platform()} ${os.arch()} ${os.release()}</td></tr>
+<tr><th>Uptime</th><td>${os.uptime()} ${util.inspect(os.loadavg())}</td></tr>
+<tr><th>Memory</th><td>total: ${os.totalmem()} free: ${os.freemem()}</td></tr>
+<tr><th>CPU's</th><td><pre>${util.inspect(os.cpus())}</pre></td></tr>
+<tr><th>Network</th><td><pre>${util.inspect(os.networkInterfaces())}</pre></td></tr>
+</table>
+</body></html>`);
+});
+
+
+server.listen(3000);
